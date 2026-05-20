0275996a37a8986b7291606e4c2358c5b03a8fff
Lucas VAN DE WALLE <lucas07300@gmail.com>
2026-05-20 14:36:57 +0200

    [US]: Création compte trader Fixes #28 Ajout du ToString, du HashCode, CopyRight et equals

--- src/main/java/fr/utc/miage/shares/Trader.java
+++ src/main/java/fr/utc/miage/shares/Trader.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 David Navarre <David.Navarre@irit.fr>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package fr.utc.miage.shares;
 
 public class Trader {
@@ -43,6 +59,50 @@ public class Trader {
         }
         return new Trader(nom, prenom, email);
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nom == null) ? 0 : nom.hashCode());
+        result = prime * result + ((prenom == null) ? 0 : prenom.hashCode());
+        result = prime * result + ((email == null) ? 0 : email.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Trader other = (Trader) obj;
+        if (nom == null) {
+            if (other.nom != null)
+                return false;
+        } else if (!nom.equals(other.nom))
+            return false;
+        if (prenom == null) {
+            if (other.prenom != null)
+                return false;
+        } else if (!prenom.equals(other.prenom))
+            return false;
+        if (email == null) {
+            if (other.email != null)
+                return false;
+        } else if (!email.equals(other.email))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Trader [nom=" + nom + ", prenom=" + prenom + ", email=" + email + "]";
+    }
+
+    
 
 
 }
