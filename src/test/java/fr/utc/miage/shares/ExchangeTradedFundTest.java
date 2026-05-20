d48ab34ca648864f93039d4fc9e53b5290774fc4
Lucas VAN DE WALLE <lucas07300@gmail.com>
2026-05-20 16:28:26 +0200

    [US]: Consultation répartition ETF Fixes #4 1er Implementation de test

--- src/test/java/fr/utc/miage/shares/ExchangeTradedFundTest.java
+++ src/test/java/fr/utc/miage/shares/ExchangeTradedFundTest.java
@@ -1,5 +1,28 @@
 package fr.utc.miage.shares;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
 public class ExchangeTradedFundTest {
+
+    private final Company COMPANY = new Company("Company");
+    private final ExchangeTradedFund CORRECT_ETF = new ExchangeTradedFund("ETF");
+    private final ExchangeTradedFund INCORRECT_ETF = new ExchangeTradedFund(null);
+
+    private final Map<Company, Float> REPARTITION = Map.of(COMPANY, 0.5f);
+
+    @Test
+    public void TestgetRepartitionsWithValideParam(){
+       assertEquals(new HashMap<>(), CORRECT_ETF.getRepartitions());
+    }
+
+    @Test
+    public void TestgetRepartitionsWithInvalideParam(){
+        assertEquals(new HashMap<>(), INCORRECT_ETF.getRepartitions());
+    }
     
+
 }
