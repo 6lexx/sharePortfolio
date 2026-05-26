f5e1f0b2655b02cb527930336852465f09b5c9de
Matthys Verbregue <matthys.verbregue@gmail.com>
2026-05-26 16:18:53 +0200

    Ecriture de la méthode TestSellActionThrowExceptionIfQuantityNull

--- src/test/java/fr/utc/miage/shares/PortfolioTest.java
+++ src/test/java/fr/utc/miage/shares/PortfolioTest.java
@@ -6,14 +6,14 @@ import org.junit.jupiter.api.Test;
 import java.util.Map;
 
 public class PortfolioTest {
-
-    private static final int ACTUAL_QUANTITY = 1;
     private static final int ACTUAL_QUANTITY_NULL = 0;
+    private static final int ACTUAL_QUANTITY = 1;
+    private static final int ACTUAL_MORE_QUANTITY = 2;
     private static final String ACTUAL_ACTION_LIBELLE = "ACTION LIBELLE TEST";
     private static final String ACTUAL_COMPAGNY_NAME = "Company Test";
 
     /**
-     * Vérifie qu'un achat d'une action simple depuis un portefeuille vide fonctionne correctement.
+     * Vérifie qu'un achat d'une action depuis un portefeuille vide fonctionne correctement.
      */
     @Test
     void TestBuyActionFromEmptyPortfolio(){
@@ -44,7 +44,7 @@ public class PortfolioTest {
      * invalide (zéro ou négative).
      */
     @Test
-    void TestBuyActionThrowExceptionIfQuantityNegative(){
+    void TestBuyActionThrowExceptionIfQuantityNull(){
         Portfolio p = new Portfolio();
         Company c = new Company(ACTUAL_COMPAGNY_NAME);
         ActionSimple a = new ActionSimple(ACTUAL_ACTION_LIBELLE, c);
@@ -54,4 +54,17 @@ public class PortfolioTest {
         );
     }
 
+    /**
+     * Vérifie que la vente d'une action depuis un portefeuille fonctionne correctement.
+     */
+    @Test
+    void TestSellActionThrowExceptionIfQuantityNull(){
+        Portfolio p = new Portfolio();
+        Company c = new Company(ACTUAL_COMPAGNY_NAME);
+        ActionSimple a = new ActionSimple(ACTUAL_ACTION_LIBELLE, c);
+
+        Assertions.assertThrows(IllegalArgumentException.class, () ->
+                p.sellAction(a, ACTUAL_QUANTITY_NULL)
+        );
+    }
 }
