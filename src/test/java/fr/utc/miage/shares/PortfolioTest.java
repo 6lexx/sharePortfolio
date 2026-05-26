00fcf5577c13c811903cd6db688f8fe0618f17b6
Matthys Verbregue <matthys.verbregue@gmail.com>
2026-05-26 16:25:21 +0200

    Ecriture de la méthode TestSellActionThrowExceptionIfQuantityExceedsOwnedActions

--- src/test/java/fr/utc/miage/shares/PortfolioTest.java
+++ src/test/java/fr/utc/miage/shares/PortfolioTest.java
@@ -8,7 +8,7 @@ import java.util.Map;
 public class PortfolioTest {
     private static final int ACTUAL_QUANTITY_NULL = 0;
     private static final int ACTUAL_QUANTITY = 1;
-    private static final int ACTUAL_MORE_QUANTITY = 2;
+    private static final int ACTUAL_QUANTITY_EXCEEDING = 2;
     private static final String ACTUAL_ACTION_LIBELLE = "ACTION LIBELLE TEST";
     private static final String ACTUAL_COMPAGNY_NAME = "Company Test";
 
@@ -79,6 +79,21 @@ public class PortfolioTest {
 
         Assertions.assertThrows(IllegalArgumentException.class, () ->
                 p.sellAction(a, ACTUAL_QUANTITY)
+        );
+    }
+
+    /**
+     * Vérifie qu'une exception est levée lors de la vente d'une quantité supérieure aux actions possédées.
+     */
+    @Test
+    void TestSellActionThrowExceptionIfQuantityExceedsOwnedActions(){
+        Portfolio p = new Portfolio();
+        Company c = new Company(ACTUAL_COMPAGNY_NAME);
+        ActionSimple a = new ActionSimple(ACTUAL_ACTION_LIBELLE, c);
+
+        p.buyAction(a, ACTUAL_QUANTITY);
+        Assertions.assertThrows(IllegalArgumentException.class, () ->
+                p.sellAction(a, ACTUAL_QUANTITY_EXCEEDING)
         );
     }
 }
